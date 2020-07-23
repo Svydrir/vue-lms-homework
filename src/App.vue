530e5a11f22846397fbb204b77bc2371d0f4f08f
Sergei Chernik <sergey.chernik@maximumtest.ru>
2020-07-23 15:32:28 +0000

    Добавление страницы расписания
    Добавлен статичный файл с расписанием
    Добавлена страницы расписния
    Страница расписания добавлена в роутер

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="menu">
+      <navigation-menu/>
+    </div>
+    <div class="content">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
+import NavigationMenu from './components/NavigationMenu';
+
 export default {
   name: 'App',
+  components: {
+    NavigationMenu,
+  },
 };
 </script>
 
 <style>
-
+.menu{
+  float: left;
+  width: 15%;
+  position: fixed;
+}
+.content{
+  float: right;
+  width: 85%;
+}
 </style>
---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Разделы</h3>
+    <ul>
+      <li>
+        <router-link
+          to="/"
+          exact
+        >
+          Главная
+        </router-link>
+      </li>
+      <li><router-link to="/schedule">Расписание</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavigationMenu',
+};
+</script>
+
+<style scoped>
+a.router-link-active, li.router-link-active>a {
+  font-weight: bold;
+}
+</style>
