c5f2b0bdd5f6d861734c3ee4cd8b5fd81e57dbcf
Sergei Chernik <sergey.chernik@maximumtest.ru>
2020-07-26 15:48:51 +0300

    Добавление страницы урока
    Добавлены компоненты тестов \
    Добавлены ссылки на урок в расписании \

---
+++ src/components/practice/inputTest.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <form @submit.prevent="showResult">
+      <h3>{{ test.title }}</h3>
+      <ul>
+        <li>
+          <input
+            v-model="studentAnswer"
+            type="text"
+            @input="resetResult"
+          >
+        </li>
+      </ul>
+      <button type="submit">Ответить</button>
+      <div v-if="resultIsVisible">
+        <h4
+          v-if="studentAnswer == test.rightAnswer"
+          class="right"
+        >
+          Твой ответ "{{ studentAnswer }}" - верный
+        </h4>
+        <h4
+          v-else
+          class="wrong"
+        >
+          Твой ответ "{{ studentAnswer }}" - неверный
+        </h4>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputTest',
+  data() {
+    return {
+      studentAnswer: '',
+      resultIsVisible: false,
+      test: {
+        title: 'Укажи результат выражения 38%5',
+        rightAnswer: 3,
+      },
+    };
+  },
+  methods: {
+    showResult() {
+      if (this.studentAnswer !== '') {
+        this.resultIsVisible = true;
+        return this.resultIsVisible;
+      }
+    },
+    resetResult() {
+      this.resultIsVisible = false;
+    },
+  },
+};
+</script>
---
+++ src/pages/LessonPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Урок</h1>
+    <input-test/>
+    <radio-test/>
+    <checkbox-test/>
+  </div>
+</template>
+
+<script>
+import RadioTest from '../components/practice/radioTest';
+import InputTest from '../components/practice/inputTest';
+import CheckboxTest from '../components/practice/checkboxTest';
+
+export default {
+  name: 'LessonPage',
+  components: {
+    RadioTest,
+    InputTest,
+    CheckboxTest,
+  },
+};
+</script>
+
+<style>
+  li {
+    list-style-type: none;
+   }
+  .right {
+    color:green;
+  }
+  .wrong {
+    color:red;
+  }
+</style>
